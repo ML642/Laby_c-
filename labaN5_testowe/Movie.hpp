8733b8208f79a1b614d97a3b92989980ae9a5f4a
ML <gitforgit123@gmail.com>
2026-05-03 18:29:31 +0200

    etap 2

---
+++ labaN5_testowe/Movie.hpp
@@ -0,0 +1,107 @@
+#pragma once 
+#include <list>
+#include <variant>
+#include <optional>
+#include <string>
+
+template<typename RatingType>
+class Movie{
+  
+    public:
+        struct Director{
+        std::string name ;
+        int numberOfOscars;
+        Director(std::string name , int numberOfOscars):name(name),numberOfOscars(numberOfOscars){};
+
+    };
+        Movie<RatingType>& operator+(RatingType rate){
+            ratings.emplace_back(rate);
+            return *this;
+        };
+
+        Movie(std::string title, int year , std::variant<Director,std::string> direktor):title(title),year(year),director(direktor){ };
+        Movie(std::string title, int year , std::variant<Director,std::string> direktor,std::string opis):title(title),year(year),director(direktor),description(opis){ };
+
+        std::string getTitle()const {return this->title;}
+        int getYear()const{return this->year;}
+        std::optional<std::string> getDescription()const {return this->description;}
+        std::variant<Director,std::string> getDirector()const {return this->director;}
+        std::list<RatingType> getRatings()const {return this->ratings;}
+
+        RatingType getAverageRating()const {
+            if(this->ratings.size() == 0 )return RatingType(0);
+            RatingType average = 0;
+            for (const auto i:this->ratings )
+                {
+                    average+=i;
+                }
+            average = average / this->ratings.size();
+            return average;
+        };
+        RatingType getTopRating() const {
+            if(this->ratings.size() == 0)return RatingType(0);
+            RatingType max = -1; 
+            for (const auto i:this->ratings) 
+                {
+                    if(max < i)max=i;
+                }
+            return max;
+        };
+       
+      
+
+    private:
+        std::string title;
+        int year;
+        std::variant<Director,std::string> director;
+        std::optional<std::string> description;
+        std::list<RatingType> ratings;
+        
+
+};
+
+
+template<typename RatingType>
+std::ostream& operator<<(std::ostream& os ,const   Movie<RatingType>& movie) {
+    std::string NameoftheAuther;
+    int Tittle;
+    
+    const auto& d = movie.getDirector();
+
+if (d.index() == 0) {
+    const auto& dir = std::get<typename Movie<RatingType>::Director>(d);
+    NameoftheAuther = dir.name;
+    Tittle = dir.numberOfOscars;
+}
+else {
+    NameoftheAuther = std::get<std::string>(d);
+    Tittle = 0;
+}
+            os << movie.getTitle() <<" (" << movie.getYear() << " )"  <<", Director: "<< NameoftheAuther<< "(Oskars:"<<Tittle<<" )"<<"Avg: " <<movie.getAverageRating()<< " Top : "<<movie.getTopRating();
+            return os;
+        };
+
+template<typename RatingType>
+bool operator<(const Movie<RatingType>& other1 , const Movie<RatingType>& other2)  {
+            if(other1.getTitle()==other2.getTitle()){
+                if(other1.getYear() < other2.getYear()){
+                    return true ;
+                }
+                else {
+                    return false;
+                }
+            }
+            if(other1.getTitle() < other2.getTitle()){
+                return true;
+            }
+            return false;
+        };
+template<typename RatingType>
+bool operator==(Movie<RatingType>& other , Movie<RatingType>& other2){
+            if(other2.getTitle() == other.getTitle() && other2.getTitle() == other.getTitle()){
+                return true;
+            }
+            return false;
+}
+
+
